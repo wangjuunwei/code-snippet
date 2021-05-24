4193dc97413a0bc8170df4272248983370bd19c7
wangjunwei <wangjunwei@qbb6.com>
2021-05-24 23:06:46 +0800

    2021/05/24 完成useEffect 编写

--- src/pages/UseApiDemo/useEffect/index.tsx
+++ src/pages/UseApiDemo/useEffect/index.tsx
@@ -1,17 +1,156 @@
 import React from "react";
 import CodeWrapper from "../../../component/createCodeWrapper";
-import useState from "./useEffect.md";
+import useEffect from "./useEffect.md";
 
-const UseState: React.FC<{}> = () => {
+import {Row, Col, Card, Button} from 'antd'
+
+/**
+ * @deprecated useEffect 的 TypeScript 定义
+ *
+ * function useLayoutEffect(effect: EffectCallback, deps?: DependencyList): void;
+ */
+
+
+/**
+ * @TODO  不需要清除的Effect
+ * @description 在React 更新DOM之后额外的代码（例如：发送网络请求、手动变更DOM、记录日志）常见的无需清除操作
+ */
+const NotCleanEffect: React.FC = () => {
+    const [count, setCount] = React.useState<number>(0)
+
+    React.useEffect(() => {
+        console.log('NotCleanEffect====>effect', count)
+    })
 
     return (
-        <>我是FunUseEffect 函数</>
+        <>
+            <p>点击{count}的次数</p>
+            <Button type="primary" onClick={() => setCount(count + 1)}>
+                点击
+            </Button>
+        </>
+    )
+
+}
+
+/**
+ * @TODO  需要清除的Effect
+ * @constructor
+ */
+const NeedCleanEffect: React.FC = () => {
+
+    const [loading, setloading] = React.useState<boolean>(true)
+
+    React.useEffect(() => {
+        let mounted = true
+        let timer = setInterval(() => {
+            if (mounted) {
+                setloading(false)
+            }
+        }, 1000)
+
+        return () => {
+            mounted = false
+            clearInterval(timer)
+        }
+    })
+
+    return (
+        <>
+            <div>{loading ? <p>loading...</p> : <p>Fetched!!</p>}</div>
+            <Button type="primary">
+                点击
+            </Button>
+        </>
+    )
+}
+
+/**
+ * @TODO 同时使用多个Effect
+ * @constructor
+ */
+const MultipleEffect: React.FC = () => {
+    const [count, setCount] = React.useState<number>(0)
+
+    React.useEffect(() => {
+        console.log('MultipleEffect First====>effect', count)
+    })
+
+    React.useEffect(() => {
+        console.log('MultipleEffect Second====>effect', count)
+    })
+
+    return (
+        <>
+            <p>点击{count}的次数</p>
+            <Button type="primary" onClick={() => setCount(count + 1)}>点击+1</Button>
+        </>
+    )
+}
+
+/**
+ * @TDOO 通过跳过 Effect 进行性能优化
+ * @constructor
+ */
+const NeedJumpEffect: React.FC = () => {
+    const [count, setCount] = React.useState<number>(0)
+
+    const [value, setValue] = React.useState<number>(0)
+
+    React.useEffect(() => {
+        console.log('NeedJumpEffect ====>effect', value)
+    }, [value])
+
+
+    return (
+        <>
+            <p>无影响{count}的次数,有影响的{value}的次数</p>
+            <Button type="primary" onClick={() => setCount(count + 1)}>点击+1</Button>
+            <Button style={{marginLeft: '10px'}} type="primary" onClick={() => setValue(value + 1)}>点击+1</Button>
+        </>
+    )
+}
+
+
+/**
+ * @TODO UseEffect 清除函数
+ * @constructor
+ */
+const UseEffect: React.FC = () => {
+    return (
+        <>
+            <Row gutter={16}>
+                <Col span={8}>
+                    <Card hoverable title="不需清除的effect" bordered>
+                        <NotCleanEffect/>
+                    </Card>
+                </Col>
+                <Col span={8}>
+                    <Card hoverable title="需要清除的effect" bordered>
+                        <NeedCleanEffect/>
+                    </Card>
+                </Col>
+                <Col span={8}>
+                    <Card hoverable title="使用多个effect" bordered>
+                        <MultipleEffect/>
+                    </Card>
+                </Col>
+            </Row>
+            <Row gutter={16} style={{marginTop:'20px'}}>
+                <Col span={8}>
+                    <Card hoverable title="可以跳过effect的优化" bordered>
+                        <NeedJumpEffect/>
+                    </Card>
+                </Col>
+
+            </Row>
+        </>
     )
 }
 
 const UseEffectWrapper: React.FC = () => {
     return (
-        <CodeWrapper component={UseState} innerHtml={useState} />
+        <CodeWrapper component={UseEffect} innerHtml={useEffect}/>
     )
 }
 
--- src/pages/UseApiDemo/useEffect/useEffect.md
+++ src/pages/UseApiDemo/useEffect/useEffect.md
@@ -1,1 +1,64 @@
 ## UseEffect 的相关用法
+
+### 1、UseEffect 的作用
+
+```js
+// @description 
+通过这个Hook我们可以在React组件渲染后执行某些操作, 在默认情况下useEffect在每次渲染后都会执行
+
+需要注意的是
+
+1、使用第二个参数的优化方式，请确保数组中包含了所有外部作用域中会随时间变化并且在 effect 中使用的变量
+
+2、建议在Effect内使用的函数全都都放在Effect内部（不过纯计算等可以作为依赖）
+
+3、最差的情况可以将Effect 内部使用的函数放入useCallback 函数内
+```
+
+### 2、不需要清除的UseEffect
+
+```js
+const [count, setCount] = React.useState < number > (0)
+
+React.useEffect(() => {
+    console.log('NotCleanEffect====>effect', count)
+})
+```
+
+### 3、需要清除的UseEffect
+
+```js
+React.useEffect(() => {
+    function handleStatusChange(status) {
+        setIsOnline(status.isOnline);
+    }
+
+    // 订阅好友的状态
+    ChatAPI.subscribeToFriendStatus(props.friend.id, handleStatusChange);
+    // 指定此效果后如何清理
+    return function cleanup() {
+        // 移除订阅
+        ChatAPI.unsubscribeFromFriendStatus(props.friend.id, handleStatusChange);
+    };
+})
+```
+
+### 4、同时使用多个Effect
+
+```js
+React.useEffect(() => {
+    console.log('MultipleEffect First====>effect', count)
+})
+
+React.useEffect(() => {
+    console.log('MultipleEffect Second====>effect', count)
+})
+```
+
+### 5、通过跳过 Effect 进行性能优化
+
+```js
+ React.useEffect(() => {
+    console.log('NeedJumpEffect ====>effect', value)
+}, [value])
+```
